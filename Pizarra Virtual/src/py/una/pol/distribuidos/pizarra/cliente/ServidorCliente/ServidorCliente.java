74722ad9dfefba793f1887e7029a41cedc842434
miki95 <michelarrieur@gmail.com>
2015-05-07 23:40:04 -0400

    Funciona en LAN a medias - 0.1

--- Pizarra Virtual/src/py/una/pol/distribuidos/pizarra/cliente/ServidorCliente/ServidorCliente.java
+++ Pizarra Virtual/src/py/una/pol/distribuidos/pizarra/cliente/ServidorCliente/ServidorCliente.java
@@ -2,8 +2,17 @@
 package py.una.pol.distribuidos.pizarra.cliente.ServidorCliente;
 
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Enumeration;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import py.una.pol.distribuidos.pizarra.cliente.gui.PanelPizarra;
 import py.una.pol.distribuidos.pizarra.cliente.rmi.ClienteRMI;
@@ -24,15 +33,52 @@ public class ServidorCliente {
 		/*
 		 * Crear registro de objeto remoto
 		 */
-		try{
-			Registry registry = LocateRegistry.createRegistry(puerto);
-			registry.rebind(nombre, new InterfazServidorClienteImpl(pizarra));
+		
+			Registry registry;
+			try {
+
+				
+				/*Enumeration<?> interfaces = NetworkInterface.getNetworkInterfaces();
+				boolean registrado = false;
+				while (interfaces.hasMoreElements() && !registrado){
+					NetworkInterface interfaz = (NetworkInterface) interfaces.nextElement();
+					Enumeration<?> direcciones = interfaz.getInetAddresses();
+					while (direcciones.hasMoreElements() && !registrado) {
+						
+						try {
+							
+							String direccion = ((InetAddress)direcciones.nextElement()).getHostAddress();
+							System.setProperty("java.rmi.server.hostname", direccion );
+							registry = LocateRegistry.createRegistry(puerto);
+							
+							registry.rebind(nombre, new InterfazServidorClienteImpl(pizarra));
+							cliente.registrarCliente(nombre,direccion , puerto);
+						} catch (NotBoundException e) {
+							continue;
+						}
+					}
+				}
+				*/
+				
+				/* Aca va la ip del cliente */
+				String direccion = "127.0.0.1";
+				System.setProperty("java.rmi.server.hostname", direccion );
+				registry = LocateRegistry.createRegistry(puerto);
+				
+				registry.rebind(nombre, new InterfazServidorClienteImpl(pizarra));
+				cliente.registrarCliente(nombre,direccion , puerto);
+
+			} catch (RemoteException e) {
+				e.printStackTrace();
+				JOptionPane.showMessageDialog(new JFrame(), "Error de RMI.\n" + e.getMessage());
+				System.exit(1);
+			} catch (NotBoundException e) {
+				e.printStackTrace();
+				
+			}
 			
 			
-			cliente.registrarCliente(nombre, InetAddress.getLocalHost().getHostAddress(), puerto);
 			
-		}catch(Exception e){
-			e.printStackTrace();
-		}
+
 	}
 }
